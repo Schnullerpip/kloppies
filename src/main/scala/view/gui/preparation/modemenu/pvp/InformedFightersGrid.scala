94d78680630064b68e32c88327d0595b3d147fec
Julian <muellerj@htwg-konstanz.de>
2016-03-01 18:15:38 +0100

    bugfixing

--- src/main/scala/view/gui/preparation/modemenu/pvp/InformedFightersGrid.scala
+++ src/main/scala/view/gui/preparation/modemenu/pvp/InformedFightersGrid.scala
@@ -1,9 +1,7 @@
 package main.scala.view.gui.preparation.modemenu.pvp
 
 import javax.swing.{Timer, ImageIcon}
-
 import main.scala.model.fighter.Fighter
-
 import scala.swing.event.{MouseEntered, MouseClicked}
 import scala.swing._
 
@@ -18,31 +16,31 @@ class InformedFightersGrid(f:Seq[Fighter]) extends GridPanel(0, 6){
     else
       Seq()
   }
-  private def fightersView() =
-  //contents.clear()
-    fighters.foreach { f =>
-      val l = new Label() {
-        icon = new ImageIcon(f.image)
-        listenTo(mouse.clicks, mouse.moves)
-        reactions += {
-          case e: MouseClicked =>
-            fighter = Some(f)
-          case e: MouseEntered => //TODO little speachbubble with fighters name
-        }
+  private def fightersView() = fighters.foreach { f =>
+    val l = new Label() {
+      icon = new ImageIcon(f.image)
+      listenTo(mouse.clicks, mouse.moves)
+      reactions += {
+        case e: MouseClicked =>
+          fighter = Some(f)
+        case e: MouseEntered => //TODO little speachbubble with fighters name
       }
-      contents += l
-      val timer = new Timer(1000 / f.speed, Swing.ActionListener { _ =>
-        f.images.next
-        l.icon = new ImageIcon(f.image)
-        Swing.onEDT {
-          repaint()
-          toolkit.sync()
-        }
-      })
-      timer.start()
-      timers = timer +: timers
     }
+    contents += l
+    val timer = new Timer(1000 / f.speed, Swing.ActionListener { _ =>
+      f.images.next
+      l.icon = new ImageIcon(f.image)
+      Swing.onEDT {
+        repaint()
+        toolkit.sync()
+      }
+    })
+    timer.start()
+    timers = timer +: timers
+  }
+
   fightersView()
+
   override def paint(g:Graphics2D): Unit ={
     super.paint(g)
   }
